7f4847520d7162098166b48f9a7b0b7c1f70f59f
Jhony Rivero <jhony.rivero@hotmail.com>
2020-04-13 20:34:52 -0500

    request validations of searchEngine with joi

--- src/routes/searchEngine.route.js
+++ src/routes/searchEngine.route.js
@@ -1,11 +1,16 @@
 const KoaRouter = require('koa-router')
+const searchSchemas = require('../schemas/searchEngine.schema')
+const schemaValidator = require('../utils/schemaValidator')
 const SearchEngineController = require('../controllers/searchEngine.controller')
 const yenv = require('yenv')
-
+const validator = schemaValidator({
+  params: searchSchemas.paramsSchema,
+  body: searchSchemas.bodySchema
+})
 const searhEngineRouter = new KoaRouter()
 const controller = new SearchEngineController()
 const env = yenv()
 
-searhEngineRouter.post(`${env.ENDPOINTS.SEARHENGINE}/:country/:campaign/:origin?`, controller.runSearch)
+searhEngineRouter.post(`${env.ENDPOINTS.SEARHENGINE}/:country/:campaign/:origin?`, validator, controller.runSearch)
 
 module.exports = searhEngineRouter
